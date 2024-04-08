0243f197d6db7a8dd8938a628a3da9027d0fe3b6
PriyanshuSingh155 <150503735+PriyanshuSingh155@users.noreply.github.com>
2024-04-08 11:24:44 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,6 +3,6 @@ const server=express();
 server.get('/',(req,res)=>{
     res.send("Welcome to 3200");
 });
-server.listen(3200, () => {
+server.listen(4000, () => {
     console.log("server is listening on 3200");
 });
